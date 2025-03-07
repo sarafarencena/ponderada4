a4d4b781a3dde216c06312c0f27a625324f31e91
sarafarencena <smfsapp@gmail.com>
2025-03-07 08:32:28 -0300

    feat: created basic sctructure

---
+++ projetoIndividual/src/jogo.js
@@ -0,0 +1,18 @@
+class JogoScene extends Phaser.Scene {
+    constructor() {
+        super('JogoScene');
+    }
+
+    preload() {
+
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+
+    }
+}
