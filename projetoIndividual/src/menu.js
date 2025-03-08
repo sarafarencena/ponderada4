56b1d81ae6e3ec76e56c662e58e2ae4aead60c2f
sarafarencena <smfsapp@gmail.com>
2025-03-07 23:47:03 -0300

    feat: developed menu scene

--- projetoIndividual/src/menu.js
+++ projetoIndividual/src/menu.js
@@ -1,17 +1,31 @@
 class Menu extends Phaser.Scene {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super("Menu");
+  }
 
-    preload() {
-        
-    }
+  preload() {
+    this.load.image("background", "assets/background.jpg");
+    this.load.image("play", "assets/play.png");
+  }
 
-    create() {
-        
-    }
+  create() {
+    let larguraJogo = 800;
+    let alturaJogo = window.innerHeight;
 
-    update() {
+    this.add
+      .image(larguraJogo / 2, alturaJogo / 1.7, "background")
+      .setScale(1.5);
 
-    }
+    this.newGameButton = this.add
+      .image(larguraJogo / 2, alturaJogo / 2, "play")
+      .setInteractive()
+      .setScale(0.5);
+
+    this.newGameButton.on('pointerdown', () => {
+        this.scene.stop();
+        this.scene.start('JogoScene');
+    })
+  }
+
+  update() {}
 }
