1b74dfcbd92cd874537fba9d81f5c5a5072a780b
sarafarencena <smfsapp@gmail.com>
2025-03-08 10:26:49 -0300

    feat: added images to the screen

--- projetoIndividual/src/menu.js
+++ projetoIndividual/src/menu.js
@@ -6,6 +6,8 @@ class Menu extends Phaser.Scene {
   preload() {
     this.load.image("background", "assets/background.jpg");
     this.load.image("play", "assets/play.png");
+    this.load.image("gameName", "assets/gameName.png");
+    this.load.image("commands", "assets/commands.png");
   }
 
   create() {
@@ -16,15 +18,19 @@ class Menu extends Phaser.Scene {
       .image(larguraJogo / 2, alturaJogo / 1.7, "background")
       .setScale(1.5);
 
+    this.add.image(larguraJogo / 2, alturaJogo / 4, "gameName").setScale(0.7);
+
+    this.add.image(larguraJogo / 2, alturaJogo / 1.28, "commands").setScale(0.5);
+
     this.newGameButton = this.add
       .image(larguraJogo / 2, alturaJogo / 2, "play")
       .setInteractive()
       .setScale(0.5);
 
-    this.newGameButton.on('pointerdown', () => {
-        this.scene.stop();
-        this.scene.start('JogoScene');
-    })
+    this.newGameButton.on("pointerdown", () => {
+      this.scene.stop();
+      this.scene.start("JogoScene");
+    });
   }
 
   update() {}
